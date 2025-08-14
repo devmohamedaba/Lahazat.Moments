aa41774cb9308cc8ec401d34ce9545c5539968f5
devmohamedaba <dev.mohamedabayazid@gmail.com>
2025-08-14 15:12:57 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -41,3 +41,40 @@ actv.forEach(a => {
 function toggleSize(img) {
     img.classList.toggle('enlarged');
 };
+
+// function toggleLanguage() {
+//     const elems = document.querySelectorAll('[data-en]');
+//     elems.forEach(el => {
+//         const current = el.innerText;
+//         el.innerText = el.getAttribute('data-ar');
+//         el.setAttribute('data-ar', current);
+//     });
+//     const btnlang = document.querySelector(".lang");
+//     btnlang.innerText = btnlang.innerText.includes('Arabic') ? 'Switch to English' : 'Switch to Arabic العربية';
+// }
+
+
+// function convert() {
+//     const elme = document.querySelectorAll('[data-en]');
+//     elme.forEach(el => {
+//         const currentt = el.innerText;
+//         el.innerText = el.getAttribute('data-ar');
+//         el.setAttribute('data-ar', currentt);
+//     })
+// }
+
+function setLanguage(lang) {
+    const elements = document.querySelectorAll('[data-en]');
+    elements.forEach(el => {
+        if (lang === 'ar') {
+            el.innerText = el.getAttribute('data-ar');
+        } else if (lang === 'en') {
+            el.innerText = el.getAttribute('data-en');
+        }
+    });
+    window.scrollTo({
+        left: 0,
+        top: 0,
+        behavior: "smooth",
+    });
+}
